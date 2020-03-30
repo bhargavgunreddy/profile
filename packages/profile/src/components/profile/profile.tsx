69f6218a21a3da406f18725ca2e9ce500c279d9b
Bhargav gunreddy <gbhargavreddy@gmail.com>
2020-03-30 16:13:45 -0400

    Adding sliders idea for skill set

--- packages/profile/src/components/profile/profile.tsx
+++ packages/profile/src/components/profile/profile.tsx
@@ -1,5 +1,8 @@
 
 import React from 'react';
+import {Slider} from './../slider/slider'; 
+
+
 import './profile.scss';
 
 
@@ -7,28 +10,63 @@ export const Profile= ()=>{
 
 	
 return (
-	<>
-		<table>
-			<thead>
-				<tr>
-					<td>CATEGORY</td>
-					<td>SKILLS</td>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Languages</td>
-					<td>JavaScript, Java</td>
-				</tr>
-				<tr>
-					<td>Frameworks</td>
-					<td>Angular, React</td>
-				</tr>
-			</tbody>
+	<div className= "profile-section">
+		<div className = "icons">
+			<div className = "combo">
+				<img className = "icon" alt = "javascipt-logo" src="./js.png"/>
+				<Slider type='pro'/>
+			</div>
+			<div className = "combo">
+				<img className = "icon" alt = "angular-logo" src="./angular-icon.svg"/>
+				<Slider type='avg'/>
+			</div>
+			<div className = "combo">
+				<img className = "icon" alt = "react-logo" src="./logo512.png"/>
+				<Slider type='avg'/>
+			</div>
+			<div className = "combo">
+				<img className = "icon" alt = "css-logo" src="./css.png"/>
+				<Slider type='avg'/>
+			</div>
+			<div className = "combo">
+				<img className = "icon" alt = "html-logo" src="./html5.svg"/>
+				<Slider type='avg'/>
+			</div>
+			<div className = "combo">
+				<img className = "icon" alt = "es6-logo" src="./es6.png"/>
+				<Slider type='avg'/>
+			</div>
+			
+		</div>
 
-
-		</table>
-	</>
+	</div>
 	)
 }
 
+// <img className = "icon left-rotate" src="./html5.svg"/>
+		
+// 			<img className = "icon right-rotate" src="./node.png"/>
+// 			<img className = "icon left-rotate" src="./js.png"/>
+// 			<img className = "icon right-rotate" src="./github.png"/>
+
+
+// <table>
+// 			<thead>
+// 				<tr>
+// 					<td>CATEGORY</td>
+// 					<td>SKILLS</td>
+// 				</tr>
+// 			</thead>
+// 			<tbody>
+// 				<tr>
+// 					<td>Languages</td>
+// 					<td>JavaScript, Java</td>
+// 				</tr>
+// 				<tr>
+// 					<td>Frameworks</td>
+// 					<td>Angular, React</td>
+// 				</tr>
+// 			</tbody>
+
+
+// 		</table>
---
+++ packages/profile/src/components/slider/slider.tsx
@@ -0,0 +1,75 @@
+
+import React from 'react';
+import './slider.scss';
+
+
+export const Slider = (props:any) => {
+
+
+
+	let getLabel = () =>{
+		switch(props.type){
+			case 'pro': return 'Very Strong'; 
+
+			case 'avg': return 'Strong'; 
+
+			case 'beg': return 'Beginner'; 
+		}
+	}
+
+	let getSliderClass = () =>{
+		let className = 'slider-section';
+		switch(props.type){
+			case 'pro': className+= ' pro-slider-section';break; 
+
+			case 'avg': className+= ' avg-slider-section'; break;
+
+			case 'beg': className+= ' starter-slider-section'; break;
+			
+		}
+		return className;
+	}
+
+	let sliderLabel = getLabel();
+	// let className = props.type == 'pro' ? 'pro-slider-section' : 'avg-slider-section';
+	let className = getSliderClass() ;
+
+
+	return (
+	<div className= {className}>
+
+		<span className="slider-label"> {sliderLabel}
+		 </span>
+		
+	</div>
+	)
+
+}
+
+export const ProSlider= ()=>{
+
+	
+return (
+	<div className= "pro-slider-section slider-section">
+
+		<span className="slider-label green-text"> Very Strong
+		 </span>
+		<div className="proficient">
+		</div>
+	</div>
+	)
+}
+
+export const AvgSlider= ()=>{
+
+	
+return (
+	<div className= "avg-slider-section slider-section">
+
+		<span className="slider-label orange-text"> Avg </span>
+		<div className="proficient">
+		</div>
+	</div>
+	)
+}
+
